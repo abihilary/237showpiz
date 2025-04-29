3b065ea1dec5c773b4c2c4619dbf1053924a69c7
abihilary <abihilary@gmail.com>
2025-04-29 19:35:08 +0100

    hello

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os
+
+import uvicorn
 from fastapi import FastAPI
 from app.api import subscription, notification, comments,subscriber_routes
 
@@ -12,3 +15,7 @@ app.include_router(subscriber_routes.router, prefix="/subscribers")
 @app.get("/")
 def root():
     return {"message": "Welcome to Showbiz Backend"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
