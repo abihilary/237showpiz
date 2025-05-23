7e5f9718858e341947b059d2ae1bd805bfab32ef
abihilary <abihilary@gmail.com>
2025-05-23 20:52:56 +0100

    updates

--- app/api/models/Comment.py
+++ app/api/models/Comment.py
@@ -1,11 +1,12 @@
 # models/comment.py
+from pydantic import BaseModel, Field
 from typing import Optional
-
-from pydantic import BaseModel
 from datetime import datetime
+import uuid
 
 
 class Comment(BaseModel):
+    comment_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     text: str
     subscriber_id: str
     comment_count: Optional[int] = None
